96bf8a4a3cde5a66fda33111490864a4e18670b5
D4intzz <81586667+Daintz@users.noreply.github.com>
2023-03-25 15:49:50 -0500

    feat(validations): :safety_vest: Add validations
    Add validation on create account

--- pages/create-account.js
+++ pages/create-account.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import {css} from '@emotion/react';
 import Layout from '../components/layout/Layout';
-import {Form, Field, InputSubmit} from '../components/UI/Form';
+import {Form, Field, InputSubmit, Error} from '../components/UI/Form';
+
+import useValidation from '../hooks/useValidator';
+import validateCreateAccount from '../validation/validateCreateAccount';
+
+const stateInitial = {
+  name: '',
+  email: '',
+  password: ''
+};
 
 export default function CreateAccount() {
+  const {values, errors, handleSubmit, handleChange, handleBlur} = useValidation(stateInitial, validateCreateAccount, createAccount);
+
+  const {name, email, password} = values;
+
+  function createAccount() {
+    console.log('Creating account...');
+  };
+
   return (
     <div>
       <Layout>
@@ -14,7 +31,9 @@ export default function CreateAccount() {
               margin-top: 5rem;
             `}
           >CreateAccount</h1>
-          <Form>
+          <Form
+            onSubmit={handleSubmit}
+          >
             <Field>
               <label htmlFor='name'>Name</label>
               <input
@@ -22,26 +41,38 @@ export default function CreateAccount() {
                 id='name'
                 placeholder='Your Name'
                 name='name'
+                value={name}
+                onChange={handleChange}
+                onBlur={handleBlur}
               />
             </Field>
+            {errors.name && <Error>{errors.name}</Error>}
             <Field>
               <label htmlFor='email'>Email</label>
               <input
                 type='email'
                 id='email'
                 placeholder='Your email'
-                email='email'
+                name='email'
+                value={email}
+                onChange={handleChange}
+                onBlur={handleBlur}
               />
             </Field>
+            {errors.email && <Error>{errors.email}</Error>}
             <Field>
               <label htmlFor='password'>Password</label>
               <input
                 type='password'
                 id='password'
                 placeholder='Your password'
-                password='password'
+                name='password'
+                value={password}
+                onChange={handleChange}
+                onBlur={handleBlur}
               />
             </Field>
+            {errors.password && <Error>{errors.password}</Error>}
 
             <InputSubmit
               type="submit"
---
+++ validation/validateCreateAccount.js
@@ -0,0 +1,21 @@
+export default function validateCreateAccount(values) {
+  let errors = {};
+
+  if(!values.name) {
+    errors.name = "The name is obligatory";
+  };
+
+  if(!values.email) {
+    errors.email = "The email is obligatory";
+  } else if(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(errors.email)) {
+    errors.email = "Email not valid";
+  };
+
+  if (!values.password) {
+    errors.password = "The password is obligatory";
+  } else if(values.password.length < 6){
+    errors.password = "Password of more than 6 characters is required";
+  }
+
+  return errors;
+};
